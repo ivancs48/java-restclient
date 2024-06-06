cec08cd209c7340056acc2e507897b35685ccf3a
Ivan Calderon <ivancs48@gmail.com>
2024-06-05 20:38:56 -0500

    primer commit

---
+++ src/main/java/com/calderon/JavaRestclientApplication.java
@@ -0,0 +1,13 @@
+package com.calderon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRestclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calderon/JavaRestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calderon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
